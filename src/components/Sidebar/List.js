cfc46c3cc82c13a31f81abf66b8ea3fdb7677657
fullstackdev <streaming9663@gmail.com>
2021-03-04 22:27:57 +0200

    dynamic sidebar

--- src/components/Sidebar/List.js
+++ src/components/Sidebar/List.js
@@ -1,30 +1,19 @@
 import React from "react";
 import "./List.css";
-import Caret from "../../assets/caret.png";
-import Exchange from "../../assets/2local exchange_color.png";
+
+import ListHead from "./ListHead";
+import Data from "../../assets/SideBarItems";
 
 function List() {
 	return (
 		<div className="list-container">
-			<div className="list-head">
-				<img src={Exchange} alt="" />
-				<p className="list-title">Exchange</p>
-				<img className="caret" src={Caret} alt="" />
-			</div>
-			<div className="sublist">
-				<div className="sublist-item">
-					<span>BNB</span>
-					<span className="item-desc">Binance Coin</span>
-				</div>
-				<div className="sublist-item">
-					<span>BNB</span>
-					<span className="item-desc">Binance Coin</span>
-				</div>
-				<div className="sublist-item">
-					<span>BNB</span>
-					<span className="item-desc">Binance Coin</span>
-				</div>
-			</div>
+			{Data.map((item) => (
+				<ListHead
+					icon={item.icon}
+					title={item.title}
+					items={item.items}
+				/>
+			))}
 		</div>
 	);
 }
