d026b5fdea3625c7d0759adf672c0ea05bc809c9
uri200056 <uri200056@protonmail.com>
2021-03-04 09:09:58 +0200

    Initial project, added the navbar and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+	return (
+		<div className="footer-container">
+			® 2local 2019-2021. All rights reserved
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/Navbar/Dropdown.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Dropdown.css";
+
+function Dropdown() {
+	return (
+		<div className="dropdown-container">
+			<div>Account</div>
+			<div>Settings</div>
+			<div>Backup</div>
+			<div>Helpdesk</div>
+		</div>
+	);
+}
+
+export default Dropdown;
