5331d8f88082b317569472c548d9644ce7fd5f58
uri200056 <uri200056@protonmail.com>
2021-03-11 21:18:26 +0200

    settings-profile

--- src/components/Settings/Profile.js
+++ src/components/Settings/Profile.js
@@ -17,11 +17,60 @@ function ProfileTop() {
 	);
 }
 
-function Inputs() {
+function DateOfBirth() {
 	return (
-		<div>
-			<label htmlFor=""></label>
-			<input type="text" />
+		<div className="profile-input d-flex justify-content-between align-items-center">
+			<input type="text" className="profile-calendar-input" />
+			<i class="fas fa-calendar-alt mr-4 calendar-icon"></i>
+		</div>
+	);
+}
+
+function CountryDropdown() {
+	return (
+		<div class="dropdown">
+			<button
+				class="text-left d-flex justify-content-between align-items-center pr-4  profile-input dropdown-toggle"
+				type="button"
+				id="dropdownMenuButton"
+				data-toggle="dropdown"
+				aria-haspopup="true"
+				aria-expanded="false"
+			>
+				California, USA
+			</button>
+			<div
+				class="profile-dropdown-select dropdown-menu"
+				aria-labelledby="dropdownMenuButton"
+			>
+				<a class="dropdown-item" href="#">
+					Action
+				</a>
+				<a class="dropdown-item" href="#">
+					Another action
+				</a>
+				<a class="dropdown-item" href="#">
+					Something else here
+				</a>
+			</div>
+		</div>
+	);
+}
+
+function Inputs({ label }) {
+	return (
+		<div className="col-sm">
+			<label className="profile-label" htmlFor="">
+				{label}
+			</label>
+			<br />
+			{label === "Country" ? (
+				<CountryDropdown />
+			) : label === "Date of birth" ? (
+				<DateOfBirth />
+			) : (
+				<input className="profile-input" type="text" />
+			)}
 		</div>
 	);
 }
@@ -30,6 +79,18 @@ function Profile() {
 	return (
 		<div className="profile-container">
 			<ProfileTop />
+			<div className="mt-5 row">
+				<Inputs label="Full Name" />
+				<Inputs label="Adress" />
+			</div>
+			<div className="mt-5 row">
+				<Inputs label="Email Adress" />
+				<Inputs label="Mobile Number" />
+			</div>
+			<div className="mt-5 row">
+				<Inputs label="Country" />
+				<Inputs label="Date of birth" />
+			</div>
 		</div>
 	);
 }
