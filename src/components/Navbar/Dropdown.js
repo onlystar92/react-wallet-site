5d99aec55021982c83f5ed5699b8b236e1949505
uri200056 <uri200056@protonmail.com>
2021-03-22 06:25:27 +0200

    links fix

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,20 +1,42 @@
 import React from "react";
 import "./Dropdown.css";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setSettingsTab } from "../../slices/slice";
 
 function Dropdown({ setopen }) {
+	const dispatch = useDispatch();
 	return (
 		<div className="d-flex flex-column justify-content-between dropdown-container">
 			<div className="d-flex flex-column justify-content-between dropdown-top">
-				<div>
-					<i class="mr-2 fas fa-user"></i> Account
-				</div>
-
-				<div>
-					<i class="mr-2 fas fa-question-circle"></i> Help
-				</div>
+				<Link
+					onClick={() => {
+						setopen(false);
+						dispatch(setSettingsTab("Profile"));
+					}}
+					to="/settings"
+					className="dropdown-link"
+				>
+					<div>
+						<i class="mr-2 fas fa-user"></i> Account
+					</div>
+				</Link>
 				<Link
 					onClick={() => setopen(false)}
+					to="/help"
+					className="dropdown-link"
+				>
+					{" "}
+					<div>
+						<i class="mr-2 fas fa-question-circle"></i> Help
+					</div>
+				</Link>
+
+				<Link
+					onClick={() => {
+						setopen(false);
+						dispatch(setSettingsTab("General"));
+					}}
 					to="/settings"
 					className="dropdown-link"
 				>
