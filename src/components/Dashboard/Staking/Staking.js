5faa75cd681c250b75792c7fe93dc475db82ba44
uri200056 <uri200056@protonmail.com>
2021-03-24 07:03:18 +0200

    staking responsive

--- src/components/Dashboard/Staking/Staking.js
+++ src/components/Dashboard/Staking/Staking.js
@@ -4,37 +4,39 @@ import "./styles.css";
 
 function StakingCard({ APY, title, value, stake, total, icon, curr }) {
 	return (
-		<div className=" staking-card d-flex flex-column align-items-start justify-content-between">
-			<img src={icon} alt="" />
-			<div className="font-weight-bold mt-3">{title}</div>
-			<div className="staking-card-top mt-3">
-				<span className="font-weight-bold top-value">{value}</span>
-				<span className="ml-2 top-desc">{curr} earned</span>
+		<div className="p-2 col-sm-3 col-12">
+			<div className=" staking-card d-flex flex-column align-items-start justify-content-between">
+				<img src={icon} alt="" />
+				<div className="font-weight-bold mt-3">{title}</div>
+				<div className="staking-card-top mt-3">
+					<span className="font-weight-bold top-value">{value}</span>
+					<span className="ml-2 top-desc">{curr} earned</span>
+				</div>
+				<hr />
+				<div className="w-100 d-flex justify-content-between mb-2">
+					<span className="staking-bottom-left">APY:</span>
+					<span className="staking-bottom-right">{APY}</span>
+				</div>
+				<div className="w-100 d-flex justify-content-between mb-2">
+					<span className="staking-bottom-left">Your Stake:</span>
+					<span className="staking-bottom-right">{stake}</span>
+				</div>
+				<div className="w-100 d-flex justify-content-between mb-2">
+					<span className="staking-bottom-left">Total:</span>
+					<span className="staking-bottom-right">
+						{total} {curr}
+					</span>
+				</div>
+				<button className="staking-btn mt-3">Unlock Wallet</button>
 			</div>
-			<hr />
-			<div className="w-100 d-flex justify-content-between mb-2">
-				<span className="staking-bottom-left">APY:</span>
-				<span className="staking-bottom-right">{APY}</span>
-			</div>
-			<div className="w-100 d-flex justify-content-between mb-2">
-				<span className="staking-bottom-left">Your Stake:</span>
-				<span className="staking-bottom-right">{stake}</span>
-			</div>
-			<div className="w-100 d-flex justify-content-between mb-2">
-				<span className="staking-bottom-left">Total:</span>
-				<span className="staking-bottom-right">
-					{total} {curr}
-				</span>
-			</div>
-			<button className="staking-btn mt-3">Unlock Wallet</button>
 		</div>
 	);
 }
 
 function Staking() {
 	return (
-		<div className=" staking-container">
-			<div className="d-flex justify-content-between">
+		<div className="staking-container ">
+			<div className="row gx-5">
 				<StakingCard
 					APY="122.15%"
 					title="DODO POOL"
