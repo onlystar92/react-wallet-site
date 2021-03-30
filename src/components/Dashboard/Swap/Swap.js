e8cdff4fdd7aa4554b9f4f2f50bf8ad99530aebf
uri200056 <uri200056@protonmail.com>
2021-03-30 19:59:53 +0300

    swap ui

--- src/components/Dashboard/Swap/Swap.js
+++ src/components/Dashboard/Swap/Swap.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./styles.css";
 import ETH from "../../../assets/ETH.svg";
 import Bdot from "../../../assets/Group 7925.svg";
@@ -14,6 +14,19 @@ function BlockElement({ back, imgURL, unit, totalAmount, realAmount }) {
 			<p className="info">{totalAmount}<span>{realAmount + " USD"}</span></p>
 		</div>
 	</div>);
+}
+
+function NumberController({dValue, step, divNum=1, suffix=""}){
+	const [cValue, setCValue] = useState(dValue);
+	return(
+		<>
+			<span className="bottom-text">{cValue/divNum + suffix}</span>
+			<span className="spinner-button">
+				<i className="fa fa-caret-up spinner-up" onClick={()=>setCValue(cValue + step)}/>
+				<i className="fa fa-caret-down spinner-down" onClick={()=>setCValue(cValue - step)}/>
+			</span>
+		</>
+	)
 }
 
 function Swap() {
@@ -36,7 +49,8 @@ function Swap() {
 							<span className="bottom-text">0.1%</span>
 							<span className="bottom-text lft-margin">0.5%</span>
 							<span className="bottom-text active lft-margin">1.0%</span>
-							<span className="bottom-text lft-margin">1.0%</span>
+							{/* <span className="bottom-text lft-margin">1.0%</span> */}
+							<NumberController step={1} dValue={10} divNum={10} suffix="%" />
 						</p>
 					</div>
 				</div>
@@ -59,7 +73,8 @@ function Swap() {
 							Transaction Deadline
 				</h3>
 						<p>
-							<span className="bottom-text">20</span>
+							{/* <span className="bottom-text">20</span> */}
+							<NumberController step={1} dValue={20} divNum={1} />
 							<span className="bottom-text lft-margin">Minutes</span>
 						</p>
 					</div>
@@ -73,7 +88,8 @@ function Swap() {
 					<span className="bottom-text">0.1%</span>
 					<span className="bottom-text lft-margin">0.5%</span>
 					<span className="bottom-text active lft-margin">1.0%</span>
-					<span className="bottom-text lft-margin">1.0%</span>
+					{/* <span className="bottom-text lft-margin">1.0%</span> */}
+					<NumberController step={1} dValue={10} divNum={10} suffix="%" />
 				</p>
 			</div>
 			<div className="mobile-bottom-info">
@@ -81,7 +97,8 @@ function Swap() {
 					Transaction Deadline
 				</h3>
 				<p>
-					<span className="bottom-text">20</span>
+					{/* <span className="bottom-text">20</span> */}
+					<NumberController step={1} dValue={20} divNum={1} />
 					<span className="bottom-text lft-margin">Minutes</span>
 				</p>
 			</div>
